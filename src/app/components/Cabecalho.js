c8d28bfcde5e702dcc746f623aeb8b615c089d3c
Guilherme Caetano <gcml1144@gmail.com>
2024-08-19 10:37:17 -0300

    introdution class

---
+++ src/app/components/Cabecalho.js
@@ -0,0 +1,13 @@
+export default function Cabecalho() {
+
+    return (
+        <>
+            <div>
+            <h1>Cabeçalho</h1>
+            <p>Esse é o cabeçalho dessa página
+
+            </p>
+            </div>
+        </>
+    )
+}
